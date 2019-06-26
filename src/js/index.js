2394482973d78837cdeea0b4acd71322e6794eb4
trscart <you@example.com>
2019-06-26 12:32:16 +0200

    aggiunta animazione di loading

--- src/js/index.js
+++ src/js/index.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
         }
 
         $('.stip-hideMe').each(function (i) { // appear effect on scrool
-            if ($(window).scrollTop() + $(window).height() > $(this).offset().top + $(this).outerHeight() - 10) {
+            if ($(window).scrollTop() + $(window).height() > $(this).offset().top + $(this).outerHeight() - 30) {
                 $(this).animate({ 'opacity': '1' }, 500);
             }
         });
@@ -29,4 +29,35 @@ $(document).ready(function () {
     $(".stip-demoRequest").click(function () {
 
     });
+
+    //load animation
+    $(".stip-loading").delay(3000).animate({ 'opacity': '0' }, 500)
+
+    anime({
+        targets: '.stip-loadLogo',
+        scale: ["1", "1.05", "1"],
+        easing: 'easeInOutQuint',
+        loop: true,
+        duration: 1800
+    });
+
+    anime({
+        targets: '.stip-rect',
+        scaleY: ["1", "0.7", "1.05", "1"],
+        easing: 'easeInOutQuint',
+        loop: true,
+        delay: 300,
+        duration: 1500
+    });
+
+    anime({
+        targets: '.stip-spunta',
+        scale: ["1", "0.7", "1"],
+        fill: ["#4384f1", "#FD6B15", "#4384f1"],
+        rotateY: ["0", "360"],
+        easing: 'easeInOutExpo',
+        loop: true,
+        delay: 600,
+        duration: 1200
+    })
 });
