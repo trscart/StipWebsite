d42a67d663934c19c55e25efb9725d4aad7f5683
trscart <you@example.com>
2019-07-24 18:33:56 +0200

    hide loading animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-//loading animation
+/*loading animation
 if (sessionStorage.getItem('dontLoad') == null) { // do loading animation only one time 
     $(".stip-loading").delay(3000).animate({ 'opacity': '0' }, 500)
     setTimeout(function () { $(".stip-loading").addClass('z-index-low') }, 3500);
@@ -6,8 +6,38 @@ if (sessionStorage.getItem('dontLoad') == null) { // do loading animation only o
 } else {
     $(".stip-loading").hide()
 }
+*/
 
 $(document).ready(function () {
+    /* logo animation
+    anime({
+        targets: '.stip-loadLogo',
+        scale: ["1", "1.05", "1"],
+        easing: 'easeInOutQuint',
+        loop: true,
+        duration: 1800
+    });
+
+    anime({
+        targets: '.stip-rect',
+        scaleY: ["1", "0.7", "1.05", "1"],
+        easing: 'easeInOutQuint',
+        loop: true,
+        delay: 300,
+        duration: 1500
+    });
+
+    anime({
+        targets: '.stip-spunta',
+        scale: ["1", "0.7", "1"],
+        fill: ["#4384f1", "#FD6B15", "#4384f1"],
+        rotateY: ["0", "360"],
+        easing: 'easeInOutExpo',
+        loop: true,
+        delay: 600,
+        duration: 1200
+    })
+    */
 
     // change menu icon color in blog section
     if ($(location).attr('href').includes("blog")) {
@@ -268,34 +298,6 @@ $(document).ready(function () {
         $(".stip-closeReprompt").click(function () {
             $(".stip-reprompt-container").css("display", "none")
         })
-    })
-
-    anime({
-        targets: '.stip-loadLogo',
-        scale: ["1", "1.05", "1"],
-        easing: 'easeInOutQuint',
-        loop: true,
-        duration: 1800
-    });
-
-    anime({
-        targets: '.stip-rect',
-        scaleY: ["1", "0.7", "1.05", "1"],
-        easing: 'easeInOutQuint',
-        loop: true,
-        delay: 300,
-        duration: 1500
-    });
-
-    anime({
-        targets: '.stip-spunta',
-        scale: ["1", "0.7", "1"],
-        fill: ["#4384f1", "#FD6B15", "#4384f1"],
-        rotateY: ["0", "360"],
-        easing: 'easeInOutExpo',
-        loop: true,
-        delay: 600,
-        duration: 1200
     })
 
     // blog card compile PROVA
