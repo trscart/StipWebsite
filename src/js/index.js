bf28705081a0de34a87ac98070b2e4c8f712a0da
trscart <you@example.com>
2019-07-26 17:17:24 +0200

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -39,46 +39,7 @@ anime({
 */
 
 $(document).ready(function () {
-
-    /* analytics, facebook and cookies */
-    (function (w, d, s, l, i) {
-        w[l] = w[l] || []; w[l].push({
-            'gtm.start':
-                new Date().getTime(), event: 'gtm.js'
-        }); var f = d.getElementsByTagName(s)[0],
-            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-PJZM6PN')
-
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
-    gtag('config', 'UA-77012119-1');
-
-    !function (f, b, e, v, n, t, s) {
-        if (f.fbq) return; n = f.fbq = function () {
-            n.callMethod ?
-                n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-        };
-        if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
-        n.queue = []; t = b.createElement(e); t.async = !0;
-        t.src = v; s = b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t, s)
-    }(window, document, 'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '326854251303193');
-    fbq('track', 'PageView');
-    /* end analytics, facebook and cookies */
-
-    if (!$(location).attr('href').includes("contacts") && !$(location).attr('href').includes("privacy-policy")) {
-        AOS.init({
-            duration: 500,
-            easing: 'ease-in-out',
-            disable: false
-        });
-    }
-
-    console.log("here1")
+    console.log("here")
 
     // change menu icon color in blog section
     if ($(location).attr('href').includes("blog")) {
@@ -518,6 +479,44 @@ $(document).ready(function () {
         });
         markerIt.addListener('click', function () {
             infowindowIt.open(mapIt, markerIt);
+        });
+    }
+
+    /* analytics, facebook and cookies */
+    (function (w, d, s, l, i) {
+        w[l] = w[l] || []; w[l].push({
+            'gtm.start':
+                new Date().getTime(), event: 'gtm.js'
+        }); var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-PJZM6PN')
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'UA-77012119-1');
+
+    /*!function (f, b, e, v, n, t, s) {
+        if (f.fbq) return; n = f.fbq = function () {
+            n.callMethod ?
+                n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+        };
+        if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
+        n.queue = []; t = b.createElement(e); t.async = !0;
+        t.src = v; s = b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t, s)
+    }(window, document, 'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '326854251303193');
+    fbq('track', 'PageView');*/
+    /* end analytics, facebook and cookies */
+
+    if (!$(location).attr('href').includes("contacts") && !$(location).attr('href').includes("privacy-policy")) {
+        AOS.init({
+            duration: 500,
+            easing: 'ease-in-out',
+            disable: false
         });
     }
 });
